982b8f4dad48a2d6251e7ffaad8e475273d1f66b
Isaacpants <dave@boyermoving.com>
2020-06-04 10:37:36 -0700

    Add files via upload

---
+++ src/_04_hospital/Patient.java
@@ -0,0 +1,18 @@
+package _04_hospital;
+
+public class Patient {
+
+	public Object feelsCaredFor() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	public void checkPulse() {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	
+
+}
